5361eaf11597de522792327c38a0db3b75eaa102
Francescosommellaa <franc.sommella@gmail.com>
2024-06-10 18:47:05 +0200

    completa mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,1 +1,20 @@
+// Tutte le e-mail autorizzate
+let emailsAutorizzate = ["userone@gmail.com", "usertwo@gmail.com", "userthree@gmail.com"];
 
+// funzione per controllare l'email inserita
+function controllaEmail() {
+
+    // assegno il valore dell'input email all'email
+    let email = document.getElementById("email").value;
+
+    // eseguiamo il controllo sull'email
+    if (emailsAutorizzate.includes(email)) {
+        console.log("Accesso consentito");
+        document.getElementById("risultato").innerHTML = "Accesso consentito";
+    } else {
+        console.log("Accesso negato");
+        document.getElementById("risultato").innerHTML = "Accesso negato";
+    }
+
+
+}
